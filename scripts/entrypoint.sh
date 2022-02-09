7dbb38c215f5d8136e2812f0e679558c77508d08
G3 <tuxgal@Baymax.area51>
2022-02-09 00:59:38 -0800

    Add pre-launch hook support to run a custom command prior to starting the postfix server.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,14 @@ set -e -o pipefail
 setup_postfix() {
     echo "Setting up postfix ..."
 
+    if [[ "${PRE_LAUNCH_HOOK}" != "" ]]; then
+        echo "PRE_LAUNCH_HOOK is non-empty ..."
+        echo "PRE_LAUNCH_HOOK=\"${PRE_LAUNCH_HOOK:?}\""
+        echo "PRE_LAUNCH_HOOK_ARGS=\"${PRE_LAUNCH_HOOK_ARGS}\""
+        echo -e "Invoking Pre-launch hook ...\n\n$(realpath ${PRE_LAUNCH_HOOK:?}) ${PRE_LAUNCH_HOOK_ARGS}\n\n"
+        $(realpath ${PRE_LAUNCH_HOOK:?}) ${PRE_LAUNCH_HOOK_ARGS}
+    fi
+
     echo
 }
 
